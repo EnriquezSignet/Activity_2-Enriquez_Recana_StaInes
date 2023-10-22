ad18a12a20b3bedbfeea8ceab4dd79af054f2bef
acpr-2023 <144645620+acpr-2023@users.noreply.github.com>
2023-10-23 03:11:27 +0800

    added breeds template

--- script.js
+++ script.js
@@ -1,32 +1,46 @@
-let login = document.querySelector(".login-form");
-
-document.querySelector("#login-btn").onclick = () => {
-  login.classList.toggle("active");
-  navbar.classList.remove("active");
-};
-
-let navbar = document.querySelector(".header .navbar");
+const navbar = document.querySelector(".header .navbar");
 
 document.querySelector("#menu-btn").onclick = () => {
-  login.classList.remove("active");
   navbar.classList.toggle("active");
 };
 
 window.onscroll = () => {
-  login.classList.remove("active");
   navbar.classList.remove("active");
 };
 
 function toggleMenu() {
-  const navbar = document.getElementById("navbar");
-  navbar.classList.toggle("active");
+  const navbarElement = document.getElementById("navbar");
+  navbarElement.classList.toggle("active");
 }
 
 function checkScreenWidth() {
   const windowWidth = window.innerWidth;
 
   if (windowWidth > 768) {
-    const navbar = document.getElementById("navbar");
-    navbar.classList.remove("active");
+    const navbarElement = document.getElementById("navbar");
+    navbarElement.classList.remove("active");
   }
 }
+
+let preveiwContainer = document.querySelector(".cats_preview");
+let previewBox = preveiwContainer.querySelectorAll(".preview");
+
+document.querySelectorAll(".cats_container .product").forEach((product) => {
+  product.onclick = () => {
+    preveiwContainer.style.display = "flex";
+    let name = product.getAttribute("data-name");
+    previewBox.forEach((preview) => {
+      let target = preview.getAttribute("data-target");
+      if (name == target) {
+        preview.classList.add("active");
+      }
+    });
+  };
+});
+
+previewBox.forEach((close) => {
+  close.querySelector(".fa-times").onclick = () => {
+    close.classList.remove("active");
+    preveiwContainer.style.display = "none";
+  };
+});
